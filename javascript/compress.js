8b44a0bdf67aceba162bb5c2f7496761cc40105d
Ben Otte <otteb1@nku.edu>
2019-04-15 20:27:26 -0400

    Create compress.js

---
+++ javascript/compress.js
@@ -0,0 +1,17 @@
+const msg = "aaabbbcdddddeefg";
+
+function compress(message) {
+	let compressedLength = 0;
+  let countConsecutive = 0;
+  
+  for (let i = 0; i < message.length; i++) {
+  	countConsecutive++
+    if (message[i] !== message[i+1] || i + 1 >= message.length) {
+    	compressedLength += 1 + message.valueOf(countConsecutive).length;
+      countConsecutive = 0;
+    }
+  }
+  return compressedLength;
+}
+
+compress(msg);
