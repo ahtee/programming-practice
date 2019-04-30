c7e3abbcb679f9bb70cfcbdae913ca5cb676d787
Ben Otte <otteb1@nku.edu>
2019-04-29 21:22:44 -0400

    Update TreesPractice.js

--- javascript/TreesPractice.js
+++ javascript/TreesPractice.js
@@ -4,6 +4,19 @@ function Node(value) {
   this.right = null;
 }
 
-function BinaryTree() {
-  let root = new Node(0);
+function BinaryTree(value) {
+  let root = new Node(value);
+  
+  function insert(value) {
+    value < this.value ? this.left : insert(value);
+    value > this.value ? this.right : insert(value);
+  }
+}
+
+function traverseDFSInOrder(tree) {
+  if (this.root) {
+    traverseDFSInOrder(this.left);
+    console.log(this.value);
+    traverseDFSInOrder(this.right);
+  }
 }
