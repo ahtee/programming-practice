8c0f592fd9271682d54bde46695e235b0e678f73
Ben Otte <otteb1@nku.edu>
2019-05-07 18:56:29 -0400

    Update objectOriented.js

--- javascript/objectOriented.js
+++ javascript/objectOriented.js
@@ -15,8 +15,36 @@ Twitch.prototype = {
   removeFollow() {
     this.followers -= 1;
   }
-}
+};
 
 const name = prompt('Choose a username:');
 
 const Username = Object.create(Twitch(name));
+
+Username.prototype = {
+  this.friends = 0;
+  this.subscriptions = 0;
+};
+
+Username.prototype = {
+  addFriend() {
+    this.friends += 1;
+  }
+  
+  newSubscriber() {
+    this.subscriptions += 1;
+    this.duration = new Month();
+  }
+};
+
+function Month() {
+  this.month = currMonth;
+  
+  if (this.month === 1) {
+    this.days = 28;
+  } else if (this.month === 11) {
+    this.days = 32;
+  }
+  
+  this.days = 31
+}
